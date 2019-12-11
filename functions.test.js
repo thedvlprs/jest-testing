6efde84f57a95d526e94ff6baba7f4cb9e89707a
Fyunka <fyunkafinka@hotmail.com>
2019-12-11 21:49:11 +0500

    feat: add axios & worked w/async data scenarios

--- functions.test.js
+++ functions.test.js
@@ -49,7 +49,24 @@ test('There is no I in team', () => {
 });
 
 // Arrays
-test('Admin should be un usernames', () => {
-  usernames = ['john', 'sara'];
+test('Admin should be in usernames', () => {
+  usernames = ['john', 'sara', 'admin'];
   expect(usernames).toContain('admin');
+});
+
+// Working w/async data
+
+// Promise
+// test('User fetched name should be Leanne Graham', () => {
+//   expect.assertions(1);
+//   return functions.fetchUser().then(data => {
+//     expect(data.name).toEqual('Leanne Graham');
+//   });
+// });
+
+// Async Await
+test('User fetched name should be Leanne Graham', async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual('Leanne Graham');
 });
